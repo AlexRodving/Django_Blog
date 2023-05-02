e7496c175d00a482bb7022cea8d7ca4e0105b4e3
AlexRodving <alexrodving@gmail.com>
2023-05-02 23:12:08 +0300

    create model db,migrate,create superuser,push post

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-	{
-    	'author': 'Администратор',
-    	'title': 'Это первый пост',
-    	'content': 'Содержание первого поста.',
-    	'date_posted': '12 мая, 2022'
-	},
-	{
-    	'author': 'Пользователь',
-    	'title': 'Это второй пост',
-    	'content': 'Подробное содержание второго поста.',
-    	'date_posted': '13 мая, 2022'
-	}
-]
+# posts = [
+# 	{
+#     	'author': 'Администратор',
+#     	'title': 'Это первый пост',
+#     	'content': 'Содержание первого поста.',
+#     	'date_posted': '12 мая, 2022'
+# 	},
+# 	{
+#     	'author': 'Пользователь',
+#     	'title': 'Это второй пост',
+#     	'content': 'Подробное содержание второго поста.',
+#     	'date_posted': '13 мая, 2022'
+# 	}
+# ]
 
 def home(request):
     return render(request, 'blog/index.html')
@@ -24,7 +25,11 @@ def about(request):
     return render(request, 'blog/about.html')
 
 def blog(request): 
-    context = {
-        'posts': posts
+    # context = {
+    #     'posts': posts
+    # }
+    posts = {
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/blog.html', context)
+
+    return render(request, 'blog/blog.html', posts)
