7a07c63faaea07e5d7afd96812c1c24295e25590
Alex Rodving <rodving@xiver.ru>
2023-05-23 15:53:03 +0300

    front html

--- my_blog/blog/serializers.py
+++ my_blog/blog/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post                     #Указывваем модель БД
-        fields = ['title','anons','text','date'] #Поля из БД
+        fields = ['id','title','anons','text','date'] #Поля из БД
