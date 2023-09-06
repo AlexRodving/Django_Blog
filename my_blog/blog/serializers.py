5a437129d4ee39b010d0344d057bac2a261ca79c
AlexRodving <66390330+AlexRodving@users.noreply.github.com>
2023-09-06 19:00:45 +0300

    Update serializers.py

--- my_blog/blog/serializers.py
+++ my_blog/blog/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post                     #Указывваем модель БД
-        fields = ['id','title','anons','text','date'] #Поля из БД
+        fields = ['id','title','anons','text','date','img'] #Поля из БД
